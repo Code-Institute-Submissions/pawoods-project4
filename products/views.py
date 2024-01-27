4ab9eb107e4d4b4eb7d17dc366d62008b7b4c042
pawoods <>
2024-01-27 10:01:10 +0000

    adds products view, template and links to navbar and menu

--- products/views.py
+++ products/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category, SubCategory
 
 
 def all_products(request):
     """ Show all products, sorting and search queries """
 
+    categories = Category.objects.all()
     products = Product.objects.all()
+    sub_categories = SubCategory.objects.all()
 
     context = {
         'products': products,
+        'categories': categories,
+        'sub_categories': sub_categories,
     }
 
     return render(request, 'products/products.html', context)
--- project4/settings.py
+++ project4/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # Do not remove
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',  # Added to include MEDIA_URL
             ],
         },
     },
