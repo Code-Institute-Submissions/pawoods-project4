1e6b83746c8fff5af3c0e496bae375d23abd79eb
pawoods <>
2024-01-28 17:41:45 +0000

    adds Brand model to products app and brand field to products model

--- products/models.py
+++ products/models.py
@@ -33,9 +33,26 @@ class SubCategory(models.Model):
         return self.friendly_name
 
 
+class Brand(models.Model):
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=25, null=True, blank=True)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Product(models.Model):
     sub_category = models.ForeignKey('SubCategory', null=True,
                                      on_delete=models.SET_NULL)
+    brand = models.ForeignKey('Brand', null=True,
+                              on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
