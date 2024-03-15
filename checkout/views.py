bfad646e0e429f9577b7e0c806005bfb5aca1e0a
pawoods <pa.woods@outlook.com>
2024-03-15 18:09:06 +0000

    adds url and basic view for checkout app, adds blank checkout.html template

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 
-# Create your views here.
+
+def checkout(request):
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(
+            request, "Your basket is currently empty"
+        )
+        return redirect(reverse('products'))
+
+    template = 'checkout/checkout.html'
+
+    return render(request, template)
