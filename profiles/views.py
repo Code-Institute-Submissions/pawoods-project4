1a0ffd967940263d7acfaf8ae855b46eda74fd08
pawoods <pa.woods@outlook.com>
2024-03-19 23:20:04 +0000

    adds profile model, updates allautho base template and gets user profile for profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+from .models import UserProfile
+
 
 @login_required
 def profile(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
+    context = {
+        'profile': profile,
+    }
 
-    return render(request, template)
+    return render(request, template, context)
